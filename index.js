fdea40ea8581dda330e6700da9d60616fad5b4be
eyal gerby <eyalg535@gmail.com>
2022-04-28 14:39:22 -0400

    commit tree 09601e9f2c09b7359babe1154adaeec0f57d2134

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+const driversNew = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+function findMatching(array, string){
+    return array.filter(function(el){
+        return el.toLowerCase() == string.toLowerCase()
+    })
+
+    }
+
+    findMatching(drivers, "bobby")
+
+    function fuzzyMatch(array, string){
+        return array.filter(function(el){
+            return el.startsWith(string)
+        })
+    
+        }
+    
+        fuzzyMatch(drivers, "b")
+
+    function matchName(array, string){
+        return array.filter(function(el){
+            return el.name === string
+        })
+    }
+
+    matchName(driversNew, "Bobby")
